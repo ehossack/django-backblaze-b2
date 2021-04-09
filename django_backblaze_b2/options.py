1fb09385a31f3cb0e3accd6d92fb607817aeb89f
Étienne H <et@hossack.me>
2021-04-08 21:26:35 -0700

    feat: allow specification of a CDN url

--- django_backblaze_b2/options.py
+++ django_backblaze_b2/options.py
@@ -26,6 +26,11 @@ class SqliteAccountInfoConfig(TypedDict):
     databasePath: str
 
 
+class CDNConfig(TypedDict):
+    baseUrl: str
+    includeBucketUrlSegments: bool
+
+
 class BackblazeB2StorageOptions(TypedDict):
     """Configuration options."""
 
@@ -36,12 +41,13 @@ class BackblazeB2StorageOptions(TypedDict):
     authorizeOnInit: bool
     validateOnInit: bool
     allowFileOverwrites: bool
+    accountInfo: Optional[Union[DjangoCacheAccountInfoConfig, InMemoryAccountInfoConfig, SqliteAccountInfoConfig]]
+    forbidFilePropertyCaching: bool
+    specificBucketNames: ProxiedBucketNames
+    cdnConfig: Optional[CDNConfig]
     # see: https://b2-sdk-python.readthedocs.io/en/master/api/api.html#b2sdk.v1.B2Api.create_bucket
     nonExistentBucketDetails: Optional[Dict[str, Union[str, Dict[str, Any]]]]
     defaultFileInfo: Dict[str, Any]
-    specificBucketNames: ProxiedBucketNames
-    accountInfo: Optional[Union[DjangoCacheAccountInfoConfig, InMemoryAccountInfoConfig, SqliteAccountInfoConfig]]
-    forbidFilePropertyCaching: bool
 
 
 def getDefaultB2StorageOptions() -> BackblazeB2StorageOptions:
@@ -53,9 +59,10 @@ def getDefaultB2StorageOptions() -> BackblazeB2StorageOptions:
         "authorizeOnInit": True,
         "validateOnInit": True,
         "allowFileOverwrites": False,
-        "nonExistentBucketDetails": None,
-        "defaultFileInfo": {},
-        "specificBucketNames": {"public": None, "loggedIn": None, "staff": None},
         "accountInfo": {"type": "django-cache", "cache": "django-backblaze-b2"},
         "forbidFilePropertyCaching": False,
+        "specificBucketNames": {"public": None, "loggedIn": None, "staff": None},
+        "cdnConfig": None,
+        "nonExistentBucketDetails": None,
+        "defaultFileInfo": {},
     }
